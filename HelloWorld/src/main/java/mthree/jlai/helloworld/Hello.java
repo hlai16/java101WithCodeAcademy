675c568eaaac20cef2377b665c1024f79b00681c
hlai16 <gloriajoanlai@gmail.com>
2022-02-22 21:53:04 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/mthree/jlai/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ *@author Joan Lai
+ *email: gloriajoanlai@gmail.com
+ *date: Feb 22, 2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package mthree.jlai.helloworld; 
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, Joan!");
+    }
+}
