06280313978f2984a137ca0280b527bf33789928
hlai16 <gloriajoanlai@gmail.com>
2022-02-27 14:59:11 -0500

    OrderOfThings

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/TheOrderOfThings.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author glori
+ */
+public class TheOrderOfThings {
+    public static void main(String[] args) {
+        double number;
+        String opinion, size, age, shape, color, origin, material, purpose;
+        String noun;
+        
+        number = 5.0;
+        opinion = "AWESOME";
+        size = "teensy-weensy";
+        age = "new";
+        shape = "oblong";
+        color = "yellow";
+        origin = "Martian";
+        material = "platinum";
+        purpose = "good";
+        
+        noun = "dragons";
+        
+        System.out.println(number + " " + opinion + " " + size + " " + age + " " + shape + " " + color + " " + origin + " " + material + " " + purpose + " " + noun);
+    }
+}
