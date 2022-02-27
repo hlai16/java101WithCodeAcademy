e426f68e682c044f447d3dc69066ce1413084a17
hlai16 <gloriajoanlai@gmail.com>
2022-02-27 18:53:40 -0500

    AllAboutMe.java

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/AllAboutMe.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author glori
+ */
+public class AllAboutMe {
+    public static void main(String[] args) {
+        String name = "Joan";
+        String food = "Chocolate bar";
+        int pets = 0;
+        String dwelling = "townhouse";
+        boolean whistle = false;
+        
+        System.out.println("My name is " + name + ".");
+        System.out.println("My favourite food is " + food + ".");
+        System.out.println("I have " + pets + " pets.");
+        System.out.println("I live in a " + dwelling + ", and I love it here.");
+        System.out.println("It is " + whistle + " I know how to whistle.");
+    }
+}
