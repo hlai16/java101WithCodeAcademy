66e5342c6867ee0084a3110efe4b87f47fdaa702
hlai16 <gloriajoanlai@gmail.com>
2022-02-28 21:27:03 -0500

    addition

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author glori
+ */
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int total = one + two + three;
+        
+        System.out.println("Total: " + total);
+    }
+}
