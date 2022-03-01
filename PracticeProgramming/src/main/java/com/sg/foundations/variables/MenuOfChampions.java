f71da231bb880575e904e392fd0649f4828a478d
hlai16 <gloriajoanlai@gmail.com>
2022-02-28 19:59:39 -0500

    Menu

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/variables/MenuOfChampions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author glori
+ */
+public class MenuOfChampions {
+    public static void main(String[] args) {
+        double pizza = 3.50;
+        double cheesecake = 5.75;
+        double soda = 1.25;
+        
+        System.out.println("MENU");
+        System.out.println("$" + pizza + " ----- Pizza");
+        System.out.println("$" + cheesecake + "---- Cheesecake");
+        System.out.println("$" + soda + "------- Soda");
+    }
+}
