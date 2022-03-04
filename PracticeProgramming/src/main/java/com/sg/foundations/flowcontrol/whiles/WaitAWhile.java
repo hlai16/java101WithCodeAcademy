5cb163ca83fe769e788af135327567e70a3adac5
hlai16 <gloriajoanlai@gmail.com>
2022-03-04 15:32:45 -0500

    while loop

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/whiles/WaitAWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.flowcontrol.whiles;
+
+/**
+ *
+ * @author glori
+ */
+public class WaitAWhile {
+    public static void main(String[] args) {
+        int timeNow = 5;
+        int bedTime = 10;
+        
+        while (timeNow < bedTime) {
+            System.out.println("it's only " + timeNow + " o'clock!");
+            System.out.println("I think I'll stay up just a liiiiiiittle longer....");
+            timeNow++; //Time passes
+            
+            System.out.println("Oh. It's " + timeNow + " o'clock.");
+            System.out.println("Guess I should go to bed ...");
+        }
+    }
+}
