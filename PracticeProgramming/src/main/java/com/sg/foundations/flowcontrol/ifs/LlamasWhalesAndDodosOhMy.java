13b0c061b21029e33ee18f2c10a2b41a7a42e07d
hlai16 <gloriajoanlai@gmail.com>
2022-03-03 21:02:24 -0500

    LlamasWhalesAndDodos

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/ifs/LlamasWhalesAndDodosOhMy.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.flowcontrol.ifs;
+
+/**
+ *
+ * @author glori
+ */
+public class LlamasWhalesAndDodosOhMy {
+    public static void main(String[] args) {
+        int llamas = 20;
+        int whales = 15;
+        int dodos = 0;
+        
+        if (dodos > 0) {
+            System.out.println("Egads, I thought dodos were extinct!");
+        }
+        
+        if (llamas > whales) {
+            System.out.println("Whales may be bigger, but llamas are better, ha!");
+        }
+    }
+}
